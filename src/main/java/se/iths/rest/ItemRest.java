aca81d5ea7108d2787674e5082c67993d057b7f5
PeterJorgensen <dasbomber.jorgensen@gmail.com>
2021-11-08 13:53:35 +0100

    Should have all the basic fundamental stuff ready to start up the server for the 1st time

---
+++ src/main/java/se/iths/rest/ItemRest.java
@@ -0,0 +1,44 @@
+package se.iths.rest;
+
+import se.iths.entity.Item;
+import se.iths.service.ItemService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("item")
+public class ItemRest {
+
+    @Inject
+    ItemService itemService;
+
+    @Path("new")
+    @POST
+    public Response createItem(Item item){
+        itemService.createItem(item);
+        return Response.ok(item).build();
+    }
+
+    @Path("update")
+    @PUT
+    public Response updateItem(Item item) {
+        itemService.updateItem(item);
+        return Response.ok(item).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Item getItem(@PathParam("id") Long id) {
+        return itemService.findItemById(id);
+    }
+
+    @Path("getall")
+    @GET
+    public List<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
+
+
+}
---
+++ src/main/java/se/iths/service/ItemService.java
@@ -0,0 +1,33 @@
+package se.iths.service;
+
+import se.iths.entity.Item;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional // We tell it's in here we make transactions
+public class ItemService {
+    @PersistenceContext
+    EntityManager entityManager; // Handles getting and writing from database
+
+    public Item createItem(Item item) {
+        entityManager.persist(item);
+        return item;
+    }
+
+    public Item updateItem(Item item) {
+        entityManager.merge(item);
+        return item;
+    }
+
+    public Item findItemById(Long id) {
+        return entityManager.find(Item.class, id);
+    }
+
+    public List<Item> getAllItems() {
+        return entityManager.createQuery("SELECT i from Item i", Item.class).getResultList();
+    }
+
+}
